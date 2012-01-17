575091dba4dcf2c8ab2ca9e26bd67da519916151
John Allison <jrallison@gmail.com>
2012-01-17 15:29:30 -0500

    Remove need to redefine fragment_for

--- lib/parameterized_cache/action_view_extensions/cache_helper.rb
+++ lib/parameterized_cache/action_view_extensions/cache_helper.rb
@@ -7,22 +7,14 @@ module ActionView
           dynamic_vars.keys.each do |key|
             instance_variable_set("@#{key}", "____PC:#{key}____")
           end
-          safe_concat(fragment_for(name, options, &block))
+          fragment = fragment_for(name, options, &block)
+          fragment.gsub(/____PC:\w+____/) { |match| ParameterizedCache.get(match.gsub(/____|PC:/, "")) }
+          safe_concat(fragment)
         else
           yield
         end
 
         nil
-      end
-
-      def fragment_for(name, options, &block)
-        if controller.fragment_exist?(name, options)
-          fragment = controller.read_fragment(name, options) || ""
-          fragment.gsub(/____PC:\w+____/) { |match| ParameterizedCache.get(match.gsub(/____|PC:/, "")) }
-        else
-          fragment = controller.write_fragment(name, capture(&block), options) || ""
-          fragment.gsub(/____PC:\w+____/) { |match| ParameterizedCache.get(match.gsub(/____|PC:/, "")) }
-        end
       end
     end
   end
